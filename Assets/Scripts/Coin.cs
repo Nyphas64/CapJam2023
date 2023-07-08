b04a0b004cf1c105f8ff54c511dc760948f41cd4
aidanphelan <88056991+aaphelan121@users.noreply.github.com>
2023-07-08 16:04:45 -0500

    Coin.

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The slot in the header that relates to this coin. ORDER MATTERS!!!")]
+    GameObject coinSlot;
+
+    Sprite coinImg;
+
+    AudioSource coinSound;
+
+    private void Awake()
+    {
+        coinImg = GetComponent<SpriteRenderer>().sprite;
+        coinSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            coinSlot.GetComponent<Image>().sprite = coinImg;
+            coinSound.Play();
+            GetComponent<SpriteRenderer>().enabled = false;
+            enabled = false;
+        }
+    }
+}
