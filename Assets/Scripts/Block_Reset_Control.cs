65c2deb387b3c31fec6c2a5f38c593b88bd0499c
ziro petro <54560001+petropurdue@users.noreply.github.com>
2023-07-08 14:10:54 -0500

    Added block positions reset
    -blocks can now be reset
    -blocks explode upon reset
    -Added block selector prefab
    -Sorted assets (prefabs and stuff)
    - :eggplant:

--- Assets/Scripts/Block_Reset_Control.cs
+++ Assets/Scripts/Block_Reset_Control.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class Block_Reset_Control : MonoBehaviour
 {
+
+    public ParticleSystem particlePrefab;   
+    public ParticleSystem whiteSmoke;  
+
+    private Transform[] children;
+    private Vector3[] initialChildPositions;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void Awake()
+    {
+        //store initial child positions
+        children = new Transform[transform.childCount];
+        initialChildPositions = new Vector3[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children[i] = transform.GetChild(i);
+            initialChildPositions[i] = children[i].position;
+        }
+
+    }
+
+
     // Update is called once per frame
     void Update()
     {
+              if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetBlocks();
+        }
         
+    }
+
+    public void ResetBlocks()
+    {
+        for (int i = 0; i < children.Length; i++)
+        {
+            //spawn explosion on child 
+            // Israel/Palestine moment :flushed: 
+            if (particlePrefab != null)
+            {
+                ParticleSystem newParticleSystem = Instantiate(particlePrefab, children[i].position, Quaternion.identity);
+                newParticleSystem.Play();
+                Destroy(newParticleSystem.gameObject, newParticleSystem.main.duration); 
+            }
+
+            // Reset child object to its initial position
+            children[i].position = initialChildPositions[i];
+
+            if (whiteSmoke != null)
+            {
+                ParticleSystem newParticleSystem = Instantiate(whiteSmoke, children[i].position, Quaternion.identity);
+                newParticleSystem.Play();
+                Destroy(newParticleSystem.gameObject, newParticleSystem.main.duration); 
+            }
+
+
+        }
     }
 }
