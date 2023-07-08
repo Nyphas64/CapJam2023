121e4d2c0bd58c926d07d3aafd5edecda8e3f9d4
aidanphelan <88056991+aaphelan121@users.noreply.github.com>
2023-07-08 08:50:08 -0500

    Adding behavior to locked door

--- Assets/Scripts/LockedDoorBehavior.cs
+++ Assets/Scripts/LockedDoorBehavior.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class LockedDoorBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void ActivateCollider()
     {
-        
+        gameObject.AddComponent<PolygonCollider2D>();
+        gameObject.GetComponent<PolygonCollider2D>().isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("NEW LEVEL TIME");
+        }
     }
 }
